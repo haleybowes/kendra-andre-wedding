17fd85ee38e47b8776568a15518d84857bc42ff5
Haley Bowes <haley.bowes@loblaw.ca>
2021-03-08 19:03:10 -0500

    cleanup lint

--- src/form.js
+++ src/form.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-import { Link, withRouter } from "react-router-dom";
+import { withRouter } from "react-router-dom";
 import classNames from 'classnames';
 
 import './styles/App.scss';
