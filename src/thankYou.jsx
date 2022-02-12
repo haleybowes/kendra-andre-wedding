333119b13aebce3c6725c1e90368578d8db2c904
Haley Bowes <bramsonbowes.haley@gmail.com>
2022-02-12 17:03:45 -0500

    sup

--- src/thankYou.jsx
+++ src/thankYou.jsx
@@ -7,7 +7,7 @@ const ThankYou = () => (
 		<Nav />
 		<h1>Thank you for filling out the form!</h1>
 		<h2>We can't wait to celebrate with you.</h2>
-		<h2>If you have any questions please send an email to <span>sayhi@kendraandandre.com</span>.</h2>
+		<h2>If you have any questions please send an email to <span>kendrabramsonbowes@gmail.com</span> or <span>andremarcelbaril@gmail.com</span>.</h2>
 	</section>
 );
 
