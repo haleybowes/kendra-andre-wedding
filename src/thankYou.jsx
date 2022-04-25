93e712f7c56b388fcb0f48cc6786778d4f0615be
Haley Bowes <bramsonbowes.haley@gmail.com>
2022-04-24 21:02:49 -0400

    plz work

--- src/thankYou.jsx
+++ src/thankYou.jsx
@@ -4,10 +4,11 @@ import Nav from './nav.js';
 
 const ThankYou = () => (
 	<section className="thank-you">
-		<Nav />
-		<h1>Thank you for filling out the form!</h1>
-		<h2>We can't wait to celebrate with you.</h2>
-		<h2>If you have any questions please send an email to <span>kendrabramsonbowes@gmail.com</span> or <span>andremarcelbaril@gmail.com</span>.</h2>
+		<div>
+			<h2>Thank you for your RSVP!</h2>
+			<p>We can't wait to celebrate with you.</p>
+			<p>If you have any questions please send an email to <span>kendrabramsonbowes@gmail.com</span> or <span>andremarcelbaril@gmail.com</span>.</p>
+		</div>
 	</section>
 );
 
